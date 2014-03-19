d7aaf14ff32822d2e7a473e3cfeb1576a8dd19fc
Michael Todd <mikejtodd@gmail.com>
2014-03-19 18:18:46 -0500

    Added views for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all
+    @students = Student.order("name desc")
   end
   def show
     @student = Student.find(params[:id])
